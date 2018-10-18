c6b41a9565f0436a5feb603a0d313579f4cd9e01
kocuj.pl <contact@kocuj.pl>
2018-10-18 18:57:52 +0200

    Reformatting

--- examples/Example1/Example1.php
+++ examples/Example1/Example1.php
@@ -16,7 +16,7 @@ use Kocuj\Di\Examples\Example1\Lib\Main;
 use Kocuj\Di\Examples\Example1\Lib\OutputService;
 
 // information about example
-echo 'This is an example of adding shared services and adding them to standard services as constructor arguments.'.PHP_EOL;
+echo 'This is an example of adding shared services and adding them to standard services as constructor arguments.' . PHP_EOL;
 echo PHP_EOL;
 // autoloading
 require __DIR__ . '/../../vendor/autoload.php';
